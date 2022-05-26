ceaefa8d8b373d5333a4ae639a4bfbb14a460241
mohdanas2411 <anas06132@gmail.com>
2022-05-26 12:20:44 +0530

    First level cache

---
+++ src/main/java/com/example/SpringBootFirst/hibernateCache/ProductRepo.java
@@ -0,0 +1,4 @@
+package com.example.SpringBootFirst.hibernateCache;
+
+public interface ProductRepo {
+}
---
+++ src/main/java/com/example/SpringBootFirst/hibernateCache/ProductService.java
@@ -0,0 +1,2 @@
+package com.example.SpringBootFirst.hibernateCache;public class ProductService {
+}
